67c8c186796da5771af6c76d86d20fad8211fb00
sunyufeng <2220438231@qq.com>
2019-01-12 09:56:43 +0800

    接口完成

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mymongo1610/utils/getCollection.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/api/get/train_tickets', function(req, res, next) {
+    //console.log(req.query.data)
+    var data = req.query.data;
+    console.log(data)
+    mongodb('everyday', function(err, con, coll) {
+        if (err) {
+            return res.json({ code: 0, mes: err })
+        }
+        coll.find({ data: data }).toArray(function(error, result) {
+            if (error) {
+                return res.json({ code: 0, mes: error })
+            } else {
+                console.log(result)
+                res.json({
+                    code: 1,
+                    data: result
+                })
+            }
+        })
+    })
 });
 module.exports = router;
